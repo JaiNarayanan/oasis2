8b7fa43e81fbf73ed8f4411b4dc7dfbf8ac93aec
JaiNarayanan <47904405+JaiNarayanan@users.noreply.github.com>
2020-08-28 17:23:26 -0400

    Add files via upload

---
+++ account.php
@@ -0,0 +1,72 @@
+<html>
+<head>
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<style>
+body, html { overflow-x:hidden; }
+  </style>
+  <title>Oasis</title>
+<script src="https://kit.fontawesome.com/93fdec6f46.js" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script><script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="navbar.css" />
+<link rel="stylesheet" type="text/css" href="fonts.css" />
+<link rel="stylesheet" type="text/css" href="account.css" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+
+<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Quicksand">
+<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Assistant">
+<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Avenir">
+<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=coquette">
+<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Museo">
+
+</head>
+<body style="background-color:#F2F2F2;">
+<nav class="navStyle navbar navbar-expand-lg navbar-light" style="border-bottom:0px solid #EAEAEA">
+  <a class="navbar-brand middleNav"style=""  href="#"><img src="logo.png" width="100" height="100" alt="" loading="lazy"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="raleway navText navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="discussion.php">Discussion</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="account.php">Account</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="resources.php">Resources</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div class="row mainTab">
+    <div class="col-lg-10" style="background-color:#FFFFE6;padding-top:50px;padding-left:40px;padding-bottom:17px;padding-right:40px;">
+    <h1>Title</h1>
+    </div>
+    <div class="col-lg-2" style="padding:50px;text-align:center; background-color:#D3F5CD">
+    <img src="profile.png" width="135px"height="135px"class="profileImg" alt="Avatar">
+    <h2 style="margin-top:20px;font-size:25px;">Jane Schlatt</h2>
+    <div class="row">
+        <div class="col-lg-3"></div>
+        <div class="col-lg-6">
+        <h2 style="margin-top:12px;font-size:15px;color:#808080">West Windsor Plainsboro High School South</h2>
+        </div>
+    </div>
+
+</div>
+</div>
