16ad9471cfec60d59b0fa5fc0526c141fdce5af4
JaiNarayanan <47904405+JaiNarayanan@users.noreply.github.com>
2020-08-28 17:23:54 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<html>
+<head>
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<style>
+body, html { overflow-x:hidden; }
+  </style>
+  <title>Oasis</title>
+<script src="https://kit.fontawesome.com/93fdec6f46.js" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script><script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="navbar.css" />
+<link rel="stylesheet" type="text/css" href="fonts.css" />
+<link rel="stylesheet" type="text/css" href="home.css" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+
+<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Quicksand">
+<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Assistant">
+<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Avenir">
+<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=coquette">
+<link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Museo">
+
+</head>
+<body style="background-color:#FFFFE6;">
+<nav class="navStyle navbar navbar-expand-lg navbar-light" style="border-bottom:0px solid #EAEAEA">
+  <a class="navbar-brand middleNav"style=""  href="#"><img src="logo.png" width="100" height="100" alt="" loading="lazy"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="raleway navText navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="discussion.php">Discussion</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="account.php">Account</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="resources.php">Resources</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="row">
+    <div class="col-lg-5"></div>
+    <div class="col-lg-3 heading">
+        <h1>Welcome To Oasis!</h1>
+    </div>
+</div>
+<div class="row" style="margin-top:80px;">
+    <div class="col-lg-3"></div>
+    <div class="col-lg-6" style="margin-left:70px;">
+        <div class="row">
+            <div class="col-lg-5" style="text-align:justify">Ut enim ad minim veniam, squis sxercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+            <div class="col-lg-1"></div>
+            <div class="col-lg-5" style="text-align:justify">Ut veniam, quisnostrud sadasd asdda ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla lorem.</div>
+        </div>
+    </div>
+    </div>
+    <div class="row" style="margin-top:60px;">
+        <div class="col-lg-5"></div>
+                <div class="col-lg-1">
+        <div class="tabLine"></div>
+        </div>
+        </div>
+        <div class="row" style="margin-top:60px;">
+        <div class="col-lg-5"></div>
+        <div class="col-lg-3">
+            <div class="row">
+                <div class="col-9" style="text-align:center">Ut veniam, quisnostruduat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla lorem</div>
+            </div>  
+    </div>    
+    </div>
+  
+</body>
